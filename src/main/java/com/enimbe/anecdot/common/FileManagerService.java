d4e6a6e5458003d1079791c43ec722e2df9af6ad
예조 <berrry_99@naver.com>
2022-03-10 20:15:50 +0900

    ???

---
+++ src/main/java/com/enimbe/anecdot/common/FileManagerService.java
@@ -0,0 +1,15 @@
+package com.enimbe.anecdot.common;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public class FileManagerService {
+	
+	public static List<Map<String, Object>> fileUpload(Map<String, Object> map, List<MultipartFile> images) {
+		
+		List<Map<String, Object>> list = new ArrayList
+	}
+
+}
